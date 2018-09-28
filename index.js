9c5edbdafb09de1365f20c0acde9949068b54c99
Alexander Guy <alexander@andern.org>
2018-09-28 09:46:40 -0700

    Use the query API.

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-
 var pg = new PGState("ws://localhost:8080/", "alexander", "alexander", "mypasswd");
 
-pg.connect();
+pg.connect().then(() => {
+    console.log("we're ready to query!");
+
+    pg.simpleQuery("select random();").then((res) => {
+	console.log("got a random:", res);
+    }).catch((err) => {
+	console.log("we got an error:", err);
+    });
+
+    pg.simpleQuery("select now();").then((res) => {
+	console.log("got a time:", res);
+    }).catch((err) => {
+	console.log("we got an error:", err);
+    });
+});
