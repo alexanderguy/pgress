1b8492b177d0b71c2a4933db40a6ea2837d72fb6
Alexander Guy <alexander@andern.org>
2019-10-16 20:22:44 -0700

    Make this easier to play with.

--- test.js
+++ test.js
@@ -1,84 +1,25 @@
-var pg = new pgress.PGState("ws://localhost:8080/", "alexander", "alexander", "mypasswd");
+var formValue = function (id) {
+    var ele = document.getElementById(id);
 
-pg.connect().then(() => {
-    console.log("we're ready to query!");
+    return ele.value;
+};
 
-    pg.simpleQuery("select random();").then((res) => {
-	console.log("got a random:", res);
-    }).catch((err) => {
-	console.log("we got an error:", err);
+var execute = function () {
+    var pg = new pgress.PGState(formValue("url"), formValue("db"), formValue("username"), formValue("password"));
+
+    pg.connect().then(() => {
+	console.log("we're ready to query!");
+
+	pg.simpleQuery("select random();").then((res) => {
+	    console.log("got a random:", res);
+	}).catch((err) => {
+	    console.log("we got an error:", err);
+	});
+
+	pg.simpleQuery("select now();").then((res) => {
+	    console.log("got a time:", res);
+	}).catch((err) => {
+	    console.log("we got an error:", err);
+	});
     });
-
-    pg.simpleQuery("select broken();").then((res) => {
-	console.log("got a random:", res);
-    }).catch((err) => {
-	console.log("we got an error:", err);
-    });
-
-    pg.simpleQuery("select now();").then((res) => {
-	console.log("got a time:", res);
-    }).catch((err) => {
-	console.log("we got an error:", err);
-    });
-
-    var q1 = pg.extendedQuery("goonies");
-    console.log("parsing");
-    q1.parse("select $1")
-      .then((res) => {
-	  return q1.bind([], ["hey"], []);
-      })
-      .then((res) => {
-	  return q1.execute();
-      })
-      .then((res) => {
-	  console.log("got some results:", res);
-      })
-      .then(() => {
-	  return q1.close("portal");
-      })
-      .then(() => {
-	  return q1.bind([], ["you"], []);
-      })
-      .then((res) => {
-	  return q1.execute();
-      })
-      .then((res) => {
-	  console.log("got some results:", res);
-      })
-      .then(() => {
-	  return q1.close("portal");
-      })
-      .then(() => {
-	  return q1.bind([], ["guys"], []);
-      })
-      .then((res) => {
-	  return q1.execute();
-      })
-      .then((res) => {
-	  console.log("got some results:", res);
-	  return pg.terminate();
-      })
-      .catch((err) => {
-	  console.log("we got an error:", err);
-      });
-
-    var q2 = pg.extendedQuery("");
-    q2.parse("select * from testing")
-      .then((res) => {
-	  return q2.bind();
-      })
-      .then((res) => {
-	  return q2.execute(1);
-      })
-      .then((res) => {
-	  console.log("got some results:", res);
-	  return q2.execute(2);
-      })
-      .then((res) => {
-	  console.log("got some results:", res);
-      })
-      .catch((err) => {
-	  console.log("we got an error:", err);
-	  pg.terminate();
-      });
-});
+};
