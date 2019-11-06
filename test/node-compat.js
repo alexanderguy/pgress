755edf3133f008862b6c8354f91d906c96f647bd
Alexander Guy <alexander@andern.org>
2019-11-05 17:14:37 -0800

    Hold onto the passed details.

--- test/node-compat.js
+++ test/node-compat.js
@@ -3,6 +3,9 @@ TextEncoder = util.TextEncoder;
 TextDecoder = util.TextDecoder;
 
 // XXX - Can this come from somewhere else?
-CustomEvent = function (eventType) {
+CustomEvent = function (eventType, params) {
+    params = params || {};
+
     this.type = eventType;
+    this.detail = params.detail;
 };
