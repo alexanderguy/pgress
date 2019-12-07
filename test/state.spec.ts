88f9936dc8c2bf9b0ef70ec36592a4e39654d281
Alexander Guy <alexander@andern.org>
2019-12-06 16:25:16 -0800

    Make sure we're hooked up to the WebSocket mock.

--- test/state.spec.ts
+++ test/state.spec.ts
@@ -1,9 +1,19 @@
 import { strict as assert } from 'assert';
 import { PGState } from "../src/main";
+import { SocketMock } from "./util";
 
 describe('PGState', function() {
-    it('stateSanityCheck', function() {
+    describe("stateSanityCheck", function() {
         let state = new PGState("someURL", "someDB", "someUser", "somePW");
 
+        it('notConnected', function() {
+            assert.equal(state.conn.conn, undefined);
+        });
+
+        it('connectedMock', function() {
+            state.connect();
+            assert.notEqual(state.conn.conn, undefined);
+            assert(state.conn.conn instanceof SocketMock)
+        });
     });
 });
